c548493d7ecb0f59bcb17304ffd8d694ed08e295
WeeWooo04 <teja8919553504@gmail.com>
2024-02-18 10:17:05 +0530

    updated Unreviewed papers Repository .

--- pc/src/main/java/pc/Data/UnReviewedPapersRepository.java
+++ pc/src/main/java/pc/Data/UnReviewedPapersRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pc.Model.UnReviewedPapers;
 
 public interface UnReviewedPapersRepository extends JpaRepository<UnReviewedPapers, Long>{
-
+	Paper findByTitleAndAuthor(String title, String author);
 }
