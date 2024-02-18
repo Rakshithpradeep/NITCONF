deb9ab3ef599741b38556023a8d3362b661d6b32
KumarTeja-04 <weewooo004@gmail.com>
2024-02-18 10:18:52 +0530

    updated UnreviewedPapers Repository.added FindByTitleAndAuthor Function

--- pc/src/main/java/pc/Data/UnReviewedPapersRepository.java
+++ pc/src/main/java/pc/Data/UnReviewedPapersRepository.java
@@ -6,4 +6,5 @@ import pc.Model.UnReviewedPapers;
 
 public interface UnReviewedPapersRepository extends JpaRepository<UnReviewedPapers, Long>{
 	Paper findByTitleAndAuthor(String title, String author);
+	
 }
