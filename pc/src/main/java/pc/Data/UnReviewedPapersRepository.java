e0c4fa7ecdb4dc7d2d692991fa02b657d216e16f
KumarTeja-04 <weewooo004@gmail.com>
2024-02-18 10:48:08 +0530

    updated repo

--- pc/src/main/java/pc/Data/UnReviewedPapersRepository.java
+++ pc/src/main/java/pc/Data/UnReviewedPapersRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pc.Model.UnReviewedPapers;
 
 public interface UnReviewedPapersRepository extends JpaRepository<UnReviewedPapers, Long>{
-	Paper findByTitleAndAuthor(String title, String author);
+	UnReviewedPapers findByTitleAndAuthor(String title, String author);
 	
 }
