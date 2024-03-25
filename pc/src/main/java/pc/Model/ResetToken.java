103b179a766293d0bb57964ef71170547d19667c
Rakshith <Rakshith@Rakshith>
2024-03-25 16:04:11 +0530

    added reset token model

---
+++ pc/src/main/java/pc/Model/ResetToken.java
@@ -0,0 +1,67 @@
+package pc.Model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "reset_token")
+public class ResetToken {
+
+    private static final int EXPIRATION = 60 * 24; // 24 hours
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class)
+    private User user;
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+
+	public static int getExpiration() {
+		return EXPIRATION;
+	}
+
+	private Date expiryDate;
+
+   
+}
